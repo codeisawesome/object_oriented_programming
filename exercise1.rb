6c6300267e6f4e8792317a2400a6de4b56b3139c
Rui Dong <rui.dong.jh@gmail.com>
2016-08-10 15:21:22 -0400

    created instance method meow that outputs their name, preferred_food and meal_time

--- exercise1.rb
+++ exercise1.rb
@@ -1,12 +1,43 @@
+#created a Cat class with name, preferred_food, meal_time
 class Cat
+
   attr_accessor :name, :preferred_food, :meal_time
+
   def initialize(name, preferred_food, meal_time)
       @name = name
       @preferred_food = preferred_food
       @meal_time = meal_time
-      @two = "number2"
   end
-  def number
-    puts @two
+
+  def eats_at
+    if @meal_time < 12
+      puts @meal_time + "AM"
+    else
+      puts @meal_time + "PM"
+    end
   end
+
+  def meow
+    meal_time = []
+    if @meal_time <= 12
+      meal_time.push(@meal_time, "AM")
+      am = meal_time.join(' ')
+    else
+      pmtime = @meal_time - 12
+      meal_time.push(pmtime, "PM")
+      am = meal_time.join(' ')
+    end
+    puts "My name is " + @name + " and I eat " + @preferred_food + " at #{am}"
+  end
+
 end
+
+#Create two instances of Cat class
+
+bob = Cat.new("Bob", "Fish", 12)
+dan = Cat.new("Dan", "Shark", 11)
+jim = Cat.new("Jim", "Potato", 23)
+
+bob.meow
+dan.meow
+jim.meow
