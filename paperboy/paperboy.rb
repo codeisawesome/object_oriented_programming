29961ca7955d04a6c288dc665ed5ec6b4b33cedd
Rui Dong <rui.dong.jh@gmail.com>
2016-08-11 14:40:58 -0400

    Gonna try the count method

--- paperboy/paperboy.rb
+++ paperboy/paperboy.rb
@@ -11,6 +11,8 @@ class Paperboy
     @side = side
     @experience = 0
     @earnings = 0
+    @quota = 0
+    @daypay = 0
 
     if side != "odd" && side != "even"
       puts "please put either odd or even for side"
@@ -25,22 +27,31 @@ class Paperboy
   # end
 
   def quota
-    quota = @experience.to_i/2 + 50
-    quota
+    @quota = @experience.to_i/2 + 50
+    @quota
   end
 
   def deliver(start_address, end_address)
-    papertotal = (end_address.to_i - start_address.to_i).abs/2 + 1
-    @earnings = papertotal
-    if @earnings > quota*0.25 && @experience == 0
-      @earnings = 12.5 + (( papertotal - 50) * 0.5)
-    elsif @earnings > quota*0.25
-      @earnings += quota * 0.25 + (( papertotal - quota) * 0.5)
+    # Depending even or odd, +1 to odd to account extra house
+    if @side == "even"
+      papertotal = (end_address.to_i - start_address.to_i).abs/2
     else
-      @earnings += @earnings*0.25 - 2
+      papertotal = (end_address.to_i - start_address.to_i).abs/2 + 1
     end
-    @experience += (end_address.to_i - start_address.to_i + 1).abs/2
-    @earnings
+
+    if papertotal > @quota && @experience == 0
+      @earnings = 12.5 + (( papertotal - 50) * 0.5)
+      @daypay = 12.5 + (( papertotal - 50) * 0.5)
+    elsif papertotal > @quota
+      @daypay = @quota * 0.25 + (( papertotal - @quota) * 0.5)
+      @earnings += @daypay
+    else
+      daypay = papertotal*0.25 - 2
+      @earnings += daypay
+    end
+
+    @experience += papertotal
+    @daypay
   end
 
   def report
