caf9c920fbe703ec2b768fc931ea63c3bf20e62f
Rui Dong <rui.dong.jh@gmail.com>
2016-08-11 14:55:10 -0400

    gonna give it a try for later

--- paperboy/paperboy.rb
+++ paperboy/paperboy.rb
@@ -31,12 +31,42 @@ class Paperboy
     @quota
   end
 
+  # def deliver(start_address, end_address)
+  #   # Depending even or odd, +1 to odd to account extra house
+  #   if @side == "even"
+  #     papertotal = (end_address.to_i - start_address.to_i).abs/2
+  #   else
+  #     papertotal = (end_address.to_i - start_address.to_i).abs/2 + 1
+  #   end
+  #
+  #   if papertotal > @quota && @experience == 0
+  #     @earnings = 12.5 + (( papertotal - 50) * 0.5)
+  #     @daypay = 12.5 + (( papertotal - 50) * 0.5)
+  #   elsif papertotal > @quota
+  #     @daypay = @quota * 0.25 + (( papertotal - @quota) * 0.5)
+  #     @earnings += @daypay
+  #   else
+  #     daypay = papertotal*0.25 - 2
+  #     @earnings += daypay
+  #   end
+  #
+  #   @experience += papertotal
+  #   @daypay
+  # end
+
   def deliver(start_address, end_address)
-    # Depending even or odd, +1 to odd to account extra house
     if @side == "even"
-      papertotal = (end_address.to_i - start_address.to_i).abs/2
+      (start_address..end_address).each do |x|
+        if x.even?
+          papertotal += 1
+        end
+      end
     else
-      papertotal = (end_address.to_i - start_address.to_i).abs/2 + 1
+      (start_address..end_address).each do |x|
+        if x.odd?
+          papertotal += 1
+        end
+      end
     end
 
     if papertotal > @quota && @experience == 0
@@ -49,9 +79,6 @@ class Paperboy
       daypay = papertotal*0.25 - 2
       @earnings += daypay
     end
-
-    @experience += papertotal
-    @daypay
   end
 
   def report
