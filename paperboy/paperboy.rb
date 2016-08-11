dec6b242263f31a05319eaab29b14e890be82b16
Rui Dong <rui.dong.jh@gmail.com>
2016-08-11 13:55:08 -0400

    trying to figure out how to increase the quota and earnings with every new call of the methode .deliver

--- paperboy/paperboy.rb
+++ paperboy/paperboy.rb
@@ -3,25 +3,48 @@ class Paperboy
   class InitializationException < RuntimeError
   end
 
-  attr_accessor :name, :experience, :side
-  attr_reader :earnings
+  attr_accessor :name, :side
+  attr_reader :earnings, :experience
 
-  def initialize(name, experience, side, earnings)
-    if @name == "bob"
+  def initialize(name, side)
+    @name = name
+    @side = side
+    @experience = 0
+    @earnings = 0
+
+    if side != "odd" && side != "even"
       puts "please put either odd or even for side"
       raise InitializationException.new
     else
-      @name = name
-      @experience = experience
-      @side = side
-      @earnings = earnings
     end
   end
 
-  # def quota
-  #   quota = @experience/2 + 50
+  # # WRITER
+  # def name=(name)
+  #   @name = name
   # end
-  #
-  # def deliver(start_address, end_address)
+
+  def quota
+    quota = @experience.to_i/2 + 50
+    quota
+  end
+
+  def deliver(start_address, end_address)
+    papertotal = (end_address.to_i - start_address.to_i).abs/2 + 1
+    @earnings = papertotal
+    if @earnings > quota*0.25 && @experience == 0
+      @earnings = 12.5 + (( papertotal - 50) * 0.5)
+    elsif @earnings > quota*0.25
+      @earnings += quota * 0.25 + (( papertotal - quota) * 0.5)
+    else
+      @earnings += @earnings*0.25 - 2
+    end
+    @experience += (end_address.to_i - start_address.to_i + 1).abs/2
+    @earnings
+  end
+
+  def report
+    puts "I'm #{ name}, I've delivered #{ experience } papers and I've earned #{ @earnings} so far!"
+  end
 
 end
