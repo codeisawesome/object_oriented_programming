2ed83158b3548952f4017622a5d2832d1c8be079
Rui Dong <rui.dong.jh@gmail.com>
2016-08-13 00:44:15 -0400

    cash_register now rounds tax and total up to the nearest 0.05

--- taxproject/tax.rb
+++ taxproject/tax.rb
@@ -21,10 +21,16 @@ class Cash_register
     @shopping_cart = []
     @subtotal = 0
     @tax = 0
+    @rounded_amount = 0
   end
 
   def add_item(item)
     @shopping_cart << item
+  end
+
+  def rounding(x)
+    @rounded_amount = (x * 20).ceil / 20.0
+    puts @rounded_amount
   end
 
   def tax
@@ -36,14 +42,21 @@ class Cash_register
         @tax += x.base_price * 0.10
       end
     end
-    puts @tax
+    rounding(@tax)
   end
 
   def subtotal
     @shopping_cart.each do |x|
       @subtotal += x.base_price
     end
+
+
     puts @subtotal
+  end
+
+  def total
+     total = @subtotal + @tax
+     rounding(total)
   end
 
 end
@@ -53,7 +66,7 @@ end
 cash_register = Cash_register.new
 
 # Shopping list
-book = Item.new(book, false, false, 12.49)
+book = Item.new(book, true, false, 12.49)
 music_CD = Item.new(music_CD, false, false, 14.99)
 chocolate_bar = Item.new(chocolate_bar, false, false, 0.85)
 
@@ -64,9 +77,11 @@ cash_register.add_item(chocolate_bar)
 
 
 # outputting the results of the shopping list
-ObjectSpace.each_object(Cash_register) {|x| p x}
-ObjectSpace.each_object(Item) {|x| p x}
+# ObjectSpace.each_object(Cash_register) {|x| p x}
+# ObjectSpace.each_object(Item) {|x| p x}
 
 cash_register.subtotal
 
 cash_register.tax
+
+cash_register.total
