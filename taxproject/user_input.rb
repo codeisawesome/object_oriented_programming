73d813bbfac177228503c9442b413651b8274e8b
Rui Dong <rui.dong.jh@gmail.com>
2016-08-18 16:02:48 -0400

    getting files uploaded

--- taxproject/user_input.rb
+++ taxproject/user_input.rb
@@ -74,9 +74,9 @@ Total: 65.15
 
 
 Example 3:
-1 imported bottle of perfume at 27.99
-1 bottle of perfume at 18.99
-1 packet of headache pills at 9.75
+2 imported bottle of perfume at 27.99
+5 bottle of perfume at 18.99
+3 packet of headache pills at 9.75
 1 box of imported chocolates at 11.25
 
 
